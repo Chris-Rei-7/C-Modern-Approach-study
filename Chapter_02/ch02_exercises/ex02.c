23d8fb46279146a1a057a51d87983e96b9087a45
Chris-Rei-7 <232968410+Chris-Rei-7@users.noreply.github.com>
2026-04-22 23:47:04 +0800

    feat: 完成第二章ex01与ex02实战，部署gitignore拦截规则

---
+++ Chapter_02/ch02_exercises/ex02.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void)
+{
+    int num1, num2, num3;
+    float a,b,c;
+    printf("%d %d %d", &num1, &num2, &num3);
+    printf("%f %f %f", &a, &b, &c);
+    return 0;
+}
